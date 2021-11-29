d871f920b26a498edad54185e16d1e4bc931f8b2
Rahaf-Aljadaani <66742756+Rahaf-Aljadaani@users.noreply.github.com>
2021-11-29 12:17:56 +0000

    Add files via upload

---
+++ Guess-The-Phrase/app/src/main/java/com/example/app_cycler/MainActivity.kt
@@ -0,0 +1,145 @@
+package com.example.app_cycler
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.icu.number.NumberRangeFormatter.with
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings.Global.putString
+import android.view.Gravity.apply
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var clRoot: ConstraintLayout
+    private lateinit var sharedPreferences: SharedPreferences
+    lateinit var sendText: EditText
+    lateinit var listOfText: ArrayList<String>
+    lateinit var rvMain: RecyclerView
+    lateinit var but: Button
+    var chars = ArrayList<Char>()
+    var charsGussed = ArrayList<String>()
+    lateinit var Textview: TextView
+    lateinit var TextviewGesss: TextView
+    var repet = 0
+    var GusseText = "Hi iam Rahaf"
+    var GusseTextStar = "** *** *****"
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        sendText = findViewById(R.id.textView)
+        listOfText = ArrayList()
+
+        clRoot = findViewById(R.id.clRoot)
+        rvMain = findViewById(R.id.recacl)
+
+        rvMain.layoutManager = LinearLayoutManager(this)
+        rvMain.adapter = itemAdapter(listOfText)
+        Textview = findViewById(R.id.textView3)
+        TextviewGesss = findViewById(R.id.textView4)
+        but = findViewById(R.id.button)
+
+        Textview.text = GusseTextStar
+        charsGussed.add("Char Guessed : ")
+
+        /*    sharedPreferences = this.getSharedPreferences(
+                    getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+            val myMessage = sharedPreferences.getString("myMessage", "").toString()  // --> retrieves data from Shared Preferences
+    // We can save data with the following code
+            with(sharedPreferences.edit()) {
+                putString("myMessage", myMessage)
+                apply()
+            }*/
+    }
+
+
+    fun guess(view: View) {
+        val text = sendText.text.toString()
+        check(text)
+        sendText.text.clear()
+        rvMain.adapter!!.notifyDataSetChanged()
+    }
+
+    private fun check(text: String) {
+
+        val chars = GusseText.toCharArray()
+
+        if (repet <= 10 && !text.equals("")) {
+            repet++
+            listOfText.add("You have guess $text")
+            when {
+                text.length == 1 -> {
+                    if(checkLetterGussen(text)){
+                        listOfText.add("You can't guess again with same leter")
+                        listOfText.add("You Have ${10-repet} gusses left")
+                        return
+                    }
+                    var j = 0
+                    var find = false
+                    for (i in chars) {
+                        if (i.toLowerCase() == text[0].toLowerCase()) {
+                            GusseTextStar = GusseTextStar.substring(0, j) + text[0] + GusseTextStar.substring(j + 1)
+                            find = true
+                            listOfText.add("Right guess, $GusseTextStar")
+                            listOfText.add("You Have ${10-repet} gusses left")
+                            charsGussed.add(text)
+                        }
+                        j++
+                    }
+                    if (!find) {
+                        listOfText.add("Wrong guess, try again")
+                        listOfText.add("You Have ${10-repet}  gusses left")
+                        charsGussed.add(text)
+                    }
+                    Textview.text = GusseTextStar
+                }
+                GusseText.equals(text) -> {
+                    listOfText.add("You get it!")
+                    listOfText.add("Game is over!")
+                    charsGussed.add(text)
+                }
+                else -> {
+                    listOfText.add("Wrong guess, try again")
+                    listOfText.add("You Have ${10-repet}  gusses left")
+                    charsGussed.add(text)
+                }
+            }
+
+
+        } else if (repet > 10) {
+            listOfText.add("The answer is $GusseText")
+            listOfText.add("Game is over!")
+        } else {
+            Snackbar.make(clRoot, "Please enter some number", Snackbar.LENGTH_LONG).show()
+        }
+
+     var text =""
+      for (i in charsGussed){
+          text += " $i"
+      }
+        TextviewGesss.text = text
+    }
+
+
+    fun checkLetterGussen (ch : String) : Boolean{
+        for (i in charsGussed){
+           if(ch[0].toLowerCase() == i[0].toLowerCase()){
+               return true
+           }
+        }
+        return false
+    }
+}
+
