2f985c1b413b17aecf5a3a399c130143df0c646a
Rahaf-Aljadaani <66742756+Rahaf-Aljadaani@users.noreply.github.com>
2022-01-12 02:50:56 +0300

    Add files via upload

---
+++ POST-Request-Practice/Update/app/src/main/java/com/example/postrequestapp/APIInterface.kt
@@ -0,0 +1,21 @@
+package com.example.postrequestapp
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface APIInterface {
+    @POST("test/")
+    fun addInfo(@Body infoData : Info): Call<Info>
+
+    @GET("test/")
+    fun getAllInfo(): Call<InfoPerson>
+/*
+    @PUT("/test/{id}")
+    fun updateUser(@Path("id") id: Int, @Body userData: Info): Call<InfoPerson>
+
+    @DELETE("/test/{id}")
+    fun deleteUser(@Path("id") id: Int): Call<Void>
+
+    
+ */
+}
---
+++ POST-Request-Practice/Update/app/src/main/java/com/example/postrequestapp/itemAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.postrequestapp
+
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+
+class itemAdapter (var items: ArrayList<Info>) : RecyclerView.Adapter<itemAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        var textName: TextView
+        var textLocation: TextView
+
+        init {
+            textName= itemView.findViewById(R.id.textView)
+            textLocation= itemView.findViewById(R.id.textView2)
+        }
+    }
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): itemAdapter.ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.items_custom,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: itemAdapter.ViewHolder, position: Int) {
+        holder.textName.text =  items[position].name
+        holder.textLocation.text =  items[position].location
+
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    fun update(persons: InfoPerson){
+        items = persons
+        notifyDataSetChanged()
+    }
+}
