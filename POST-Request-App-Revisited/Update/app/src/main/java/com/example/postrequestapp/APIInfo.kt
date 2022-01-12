d5f3e9b39d0e46f861061a6ed9f37c776bd5c833
Rahaf-Aljadaani <66742756+Rahaf-Aljadaani@users.noreply.github.com>
2022-01-12 03:12:15 +0300

    Add files via upload

---
+++ POST-Request-App-Revisited/Update/app/src/main/java/com/example/postrequestapp/APIInfo.kt
@@ -0,0 +1,17 @@
+package com.example.postrequestapp
+
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class APIInfo {
+    private var retrofit: Retrofit? = null
+
+    fun getInfo(): Retrofit? {
+        retrofit = Retrofit.Builder()
+            .baseUrl("https://dojo-recipes.herokuapp.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        return retrofit
+    }
+}
---
+++ POST-Request-App-Revisited/Update/app/src/main/java/com/example/postrequestapp/MainActivity.kt
@@ -0,0 +1,144 @@
+package com.example.postrequestapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+data class Info(val name: String,val location: String,val pk: Int)
+class InfoPerson : ArrayList<Info>()
+
+
+class MainActivity : AppCompatActivity() {
+  //  lateinit var clRoot: ConstraintLayout
+    lateinit var rvMain: RecyclerView
+    lateinit var sendName: EditText
+    lateinit var sendLocation: EditText
+    lateinit var listOfInfo: ArrayList<Info>
+    lateinit var Adapter: itemAdapter
+    val apiInterface = APIInfo().getInfo()?.create(APIInterface::class.java)
+    lateinit var persons: InfoPerson
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        persons = InfoPerson()
+
+        sendName = findViewById(R.id.editText2)
+        sendLocation = findViewById(R.id.editText)
+
+
+        rvMain = findViewById(R.id.recacl)
+        Adapter = itemAdapter(persons)
+        rvMain.adapter = Adapter
+        rvMain.layoutManager = LinearLayoutManager(this)
+
+        val apiInterface = APIInfo().getInfo()?.create(APIInterface::class.java)
+
+        apiInterface?.getAllInfo()?.enqueue(object: Callback<InfoPerson> {
+            override fun onResponse(call: Call<InfoPerson>, response: Response<InfoPerson>) {
+                persons = response.body()!!
+                Adapter.update(persons)
+            }
+
+            override fun onFailure(call: Call<InfoPerson>, t: Throwable) {
+                Log.d("MAIN", "Unable to get data")
+            }
+        })
+
+
+    }
+
+    fun addInfo(view: View) {
+        apiInterface!!.addInfo(
+            Info( sendName.text.toString(),sendLocation.text.toString(), 0)
+        ).enqueue(object : Callback<Info> {
+            override fun onResponse(call: Call<Info>, response: Response<Info>) {
+                Toast.makeText(applicationContext, "User added!", Toast.LENGTH_LONG).show()
+                recreate()
+            }
+
+            override fun onFailure(call: Call<Info>, t: Throwable) {
+                Log.d("MAIN", "Something went wrong!")
+            }
+
+        })
+
+    }
+
+    fun updateAndDelet(view: View) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("To Do")
+        val inflater = layoutInflater
+        val dialogLayout = inflater.inflate(R.layout.dilog_custom, null)
+        val id = dialogLayout.findViewById<EditText>(R.id.Id)
+        val name = dialogLayout.findViewById<EditText>(R.id.Name)
+        val location = dialogLayout.findViewById<EditText>(R.id.Location)
+        var userID = 0
+        builder.setView(dialogLayout)
+        builder.setNeutralButton("Update") { dialog, which ->
+            Toast.makeText(applicationContext, "Update", Toast.LENGTH_SHORT).show()
+            if(id.text.isNotEmpty() && name.text.isNotEmpty() && location.text.isNotEmpty()){
+                try{
+                    userID = id.text.toString().toInt()
+                    apiInterface?.updateUser(
+                        userID,
+                        Info(
+                            name.text.toString(),
+                            location.text.toString(),
+                            userID
+                        )
+                    )?.enqueue(object: Callback<Info> {
+                        override fun onResponse(call: Call<Info>, response: Response<Info>) {
+                            Toast.makeText(applicationContext, "User Updated", Toast.LENGTH_LONG).show()
+                            recreate()
+
+                        }
+
+                        override fun onFailure(call: Call<Info>, t: Throwable) {
+                            Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_LONG).show()
+                        }
+                    })
+
+                }catch(e: Exception){
+                    Toast.makeText(applicationContext, "Invalid data", Toast.LENGTH_LONG).show()
+                }
+            }else{
+                Toast.makeText(applicationContext, "All fields are required", Toast.LENGTH_LONG).show()
+            }
+
+
+        }
+
+        builder.setPositiveButton("Delete") { dialog, which ->
+            try{
+                userID = id.text.toString().toInt()
+                apiInterface?.deleteUser(userID)?.enqueue(object: Callback<Void> {
+                    override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                        Toast.makeText(applicationContext, "User Deleted", Toast.LENGTH_LONG).show()
+                        recreate()
+                    }
+
+                    override fun onFailure(call: Call<Void>, t: Throwable) {
+                        Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_LONG).show()
+                    }
+                })
+            }catch(e: Exception){
+                Toast.makeText(applicationContext, "Invalid data", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        builder.show()
+    }
+
+}
